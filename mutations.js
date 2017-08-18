368dace88bb81554124af83f29a2936a470bc765
iat <i@iat.net.cn>
2017-08-18 16:59:56 +0800

    mutations

---
+++ mutations.js
@@ -0,0 +1,12 @@
+
+function mutation(arr) {
+    for (var i = 0; i < arr[1].length; i++) {
+        if (arr[0].toLowerCase().indexOf(arr[1][i].toLowerCase()) == -1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+mutation(["hello", "hey"]);
